946160667cfdf2c02bbc76d21620a58ef6ac8477
Cody-Cooper <contact@itscodycooper.com>
2015-12-10 21:13:22 -0800

    Initial commit.

---
+++ bonfires/reverse-a-string.js
@@ -0,0 +1,8 @@
+// Bonfire: Bonfire: Reverse a String
+// Challenge: http://www.freecodecamp.com/challenges/bonfire-reverse-a-string
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
