abecc2596680e0c818805b875c34b7ef312ccceb
Cody-Cooper <contact@itscodycooper.com>
2015-12-26 21:09:55 -0800

    Deleted whitespace.

--- bonfires/reverse-a-string.js
+++ bonfires/reverse-a-string.js
@@ -1,6 +1,7 @@
 // Bonfire: Reverse a String
 // Challenge: http://www.freecodecamp.com/challenges/bonfire-reverse-a-string
 
+
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
